a77a82e17df3c70f5dbfd4a79d068eb7a1b5dbc8
red2fred2 <red2fred2base@live.com>
2023-08-26 19:14:20 -0500

    Take input from cli args instead

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,28 @@
 //! [language Specification](https://compilers.cool/language/)
 
 use anyhow::{Ok, Result};
+use clap::Parser;
 
 pub mod lexer;
 
+/// Drewno Mars language lexer
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+	/// File to be lexed
+	input_file: Option<String>,
+    /// File to output token stream to
+    #[arg(short, long)]
+    token_file: String,
+
+}
+
 fn main() -> Result<()> {
-	let string = include_str!("../test.dm").to_string() + "\n";
-	lexer::lex(string.as_str(), "tokens.txt")?;
+	let args = Args::parse();
+	let path = args.input_file.unwrap();
+	let contents = std::fs::read_to_string(path)? + "\n";
+
+	lexer::lex(contents.as_str(), &args.token_file)?;
 
 	Ok(())
 }
