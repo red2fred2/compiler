323f2af1b0dfe0879978697cbdaf022ae8389dbb
red2fred2 <red2fred2base@live.com>
2023-08-26 19:29:16 -0500

    Change token spec formatting

--- src/lexer.rs
+++ src/lexer.rs
@@ -17,102 +17,49 @@ pub struct Token {
 #[derive(Logos, PartialEq)]
 #[logos(skip r"[ \t\f]+")]
 pub enum TokenEnum {
-	#[token("\n", new_line)]
-	#[regex(r"//[^\n]*\n", new_line)]
-	HELPER,
+	#[regex(r"(//[^\n]*\n|)|\n", new_line)] HELPER,
 
-	//Keywords
-	#[token("and")]
-	AND,
-	#[token("bool")]
-	BOOL,
-	#[token("class")]
-	CLASS,
-	#[token("else")]
-	ELSE,
-	#[token("today I don't feel like doing any work")]
-	EXIT,
-	#[token("false")]
-	#[token("too hot")]
-	FALSE,
-	#[token("give")]
-	GIVE,
-	#[token("if")]
-	IF,
-	#[token("int")]
-	INT,
-	#[token("24Kmagic")]
-	MAGIC,
-	#[token("or")]
-	OR,
-	#[token("perfect")]
-	PERFECT,
-	#[token("return")]
-	RETURN,
-	#[token("take")]
-	TAKE,
-	#[token("true")]
-	TRUE,
-	#[token("void")]
-	VOID,
-	#[token("while")]
-	WHILE,
-
-	// Identifiers
-	#[regex(r"[\p{L}_][\p{L}_\d]*", copy)]
-	ID(String),
-
-	// Integer literals
-	#[regex(r"\d+", copy)]
-	INTLITERAL(String),
-
-	// String literals
-	#[regex("\"[^\"]*\"", copy)]
-	STRINGLITERAL(String),
-
-	// Operators
-	#[token("=")]
-	ASSIGN,
-	#[token(",")]
-	COMMA,
-	#[token("+")]
-	CROSS,
-	#[token("-")]
-	DASH,
-	#[token("==")]
-	EQUALS,
-	#[token(">")]
-	GREATER,
-	#[token(">=")]
-	GREATEREQ,
-	#[token("{")]
-	LCURLY,
-	#[token("<")]
-	LESS,
-	#[token("<=")]
-	LESSEQ,
-	#[token("(")]
-	LPAREN,
-	#[token("!")]
-	NOT,
-	#[token("!=")]
-	NOTEQUALS,
-	#[token(":")]
-	OTHER,
-	#[token("--")]
-	POSTDEC,
-	#[token("++")]
-	POSTINC,
-	#[token("}")]
-	RCURLY,
-	#[token(")")]
-	RPAREN,
-	#[token(";")]
-	SEMICOL,
-	#[token("/")]
-	SLASH,
-	#[token("*")]
-	STAR,
+	#[token("and")] AND,
+	#[token("=")] ASSIGN,
+	#[token("bool")] BOOL,
+	#[token(",")] COMMA,
+	#[token("class")] CLASS,
+	#[token("+")] CROSS,
+	#[token("-")] DASH,
+	#[token("else")] ELSE,
+	#[token("==")] EQUALS,
+	#[token("today I don't feel like doing any work")] EXIT,
+	#[token("false")] #[token("too hot")] FALSE,
+	#[token("give")] GIVE,
+	#[token(">")] GREATER,
+	#[token(">=")] GREATEREQ,
+	#[regex(r"[\p{L}_][\p{L}_\d]*", copy)] ID(String),
+	#[token("if")] IF,
+	#[token("int")] INT,
+	#[regex(r"\d+", copy)] INTLITERAL(String),
+	#[token("{")] LCURLY,
+	#[token("<")] LESS,
+	#[token("<=")] LESSEQ,
+	#[token("(")] LPAREN,
+	#[token("24Kmagic")] MAGIC,
+	#[token("!")] NOT,
+	#[token("!=")] NOTEQUALS,
+	#[token("or")] OR,
+	#[token(":")] OTHER,
+	#[token("perfect")] PERFECT,
+	#[token("--")] POSTDEC,
+	#[token("++")] POSTINC,
+	#[token("}")] RCURLY,
+	#[token("return")] RETURN,
+	#[token(")")] RPAREN,
+	#[token(";")] SEMICOL,
+	#[token("/")] SLASH,
+	#[token("*")] STAR,
+	#[regex("\"[^\"]*\"", copy)] STRINGLITERAL(String),
+	#[token("take")] TAKE,
+	#[token("true")] TRUE,
+	#[token("void")] VOID,
+	#[token("while")] WHILE,
 }
 
 impl Debug for TokenEnum {
