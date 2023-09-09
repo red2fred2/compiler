29757d68c091118087b9ce774ec4e64d7a0f2dc3
red2fred2 <red2fred2base@live.com>
2023-09-08 22:26:18 -0500

    Superficial change to see if git switch worked

--- src/main.rs
+++ src/main.rs
@@ -8,24 +8,23 @@ use clap::Parser;
 
 pub mod lexer;
 
-/// Drewno Mars language lexer
+/// Drewno Mars language compiler
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-	/// File to be lexed
-	input_file: Option<String>,
+    /// File to be compiled
+    input_file: Option<String>,
     /// File to output token stream to
     #[arg(short, long)]
     token_file: String,
-
 }
 
 fn main() -> Result<()> {
-	let args = Args::parse();
-	let path = args.input_file.unwrap();
-	let contents = std::fs::read_to_string(path)? + "\n";
+    let args = Args::parse();
+    let path = args.input_file.unwrap();
+    let contents = std::fs::read_to_string(path)? + "\n";
 
-	lexer::lex(contents.as_str(), &args.token_file)?;
+    lexer::lex(contents.as_str(), &args.token_file)?;
 
-	Ok(())
+    Ok(())
 }
