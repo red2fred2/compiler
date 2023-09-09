4997b4db10d8101426e2c36776d53cc12b820632
red2fred2 <red2fred2base@live.com>
2023-09-08 22:55:38 -0500

    Add parser option, and remove input file optionality

--- src/main.rs
+++ src/main.rs
@@ -7,21 +7,27 @@ use anyhow::{Ok, Result};
 use clap::Parser;
 
 pub mod lexer;
+pub mod parser;
 
 /// Drewno Mars language compiler
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     /// File to be compiled
-    input_file: Option<String>,
+    input_file: String,
+
     /// File to output token stream to
     #[arg(short, long)]
     token_file: Option<String>,
+
+    // Parse flag
+    #[arg(short, long)]
+    parse: bool,
 }
 
 fn main() -> Result<()> {
     let args = Args::parse();
-    let path = args.input_file.unwrap();
+    let path = args.input_file;
     let contents = std::fs::read_to_string(path)? + "\n";
 
     lexer::lex(contents.as_str(), args.token_file)?;
