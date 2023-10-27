ccc00d99a777fc24e5650849c63dd8c0f856531f
red2fred2 <red2fred2base@live.com>
2023-10-27 02:20:22 -0500

    Add Type equivalence check

--- src/ast/type_.rs
+++ src/ast/type_.rs
@@ -8,6 +8,23 @@ pub enum Type {
     PerfectClass(Id, SourcePositionData),
 }
 
+impl Type {
+    pub fn equivalent(&self, x: &Self) -> bool {
+        match (self, x) {
+            (Type::Primitive(t1, _), Type::Primitive(t2, _))
+            | (Type::Primitive(t1, _), Type::PerfectPrimitive(t2, _))
+            | (Type::PerfectPrimitive(t1, _), Type::Primitive(t2, _))
+            | (Type::PerfectPrimitive(t1, _), Type::PerfectPrimitive(t2, _)) => t1 == t2,
+
+            (Type::Class(t1, _), Type::Class(t2, _))
+            | (Type::Class(t1, _), Type::PerfectClass(t2, _))
+            | (Type::PerfectClass(t1, _), Type::Class(t2, _))
+            | (Type::PerfectClass(t1, _), Type::PerfectClass(t2, _)) => t1.name == t2.name,
+            _ => false,
+        }
+    }
+}
+
 impl Display for Type {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
