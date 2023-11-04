e8bdc7f5e108a46a51ae680833bb5c09e487b966
red2fred2 <red2fred2base@live.com>
2023-11-04 03:11:55 -0500

    Add comments to source_position.rs and avoid panic If the source document was not set before it was used, there used to be a panic. Now, the display will result in an error instead.

--- src/source_position.rs
+++ src/source_position.rs
@@ -1,38 +1,83 @@
-use std::fmt::Display;
+//! # Source Position Display
+//! Helps in displaying source code positions
+
+use anyhow::{anyhow, Result};
+use std::fmt::{Display, Formatter};
 
 type BytePosition = usize;
 
 static mut SOURCE_DOCUMENT: Option<String> = None;
 
+/// Things with a position in the source code should implement this
 pub trait SourcePosition {
+    /// Finds the position of this object in the source code
     fn source_position(&self) -> SourcePositionData;
 }
 
+#[derive(Clone, Debug, PartialEq)]
+/// Holds data about a range of source code positions
+pub struct SourcePositionData {
+    /// Starting byte position
+    pub s: BytePosition,
+    /// Ending byte position
+    pub e: BytePosition,
+}
+
+impl Display for SourcePositionData {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        // Unwrap line and column results
+        let (Ok(start_line), Ok(start_column), Ok(end_line), Ok(end_column)) = (
+            get_line_number(self.s),
+            get_column(self.s),
+            get_line_number(self.e),
+            get_column(self.e),
+        ) else {
+            return Err(std::fmt::Error {});
+        };
+
+        // Write it out
+        write!(
+            f,
+            "[{},{}]-[{},{}]",
+            start_line, start_column, end_line, end_column
+        )
+    }
+}
+
+/// Sets the source code document being worked on
 pub fn set_document(contents: &String) {
     unsafe { SOURCE_DOCUMENT = Some(contents.clone()) }
 }
 
-fn get_column(pos: BytePosition) -> usize {
-    pos - get_line_start(pos) + 1
+/// Finds the column of a given source code byte position
+fn get_column(position: BytePosition) -> Result<usize> {
+    Ok(position - get_line_start(position)? + 1)
 }
 
-fn get_document() -> &'static String {
-    unsafe { SOURCE_DOCUMENT.as_ref().unwrap() }
+/// Gets a static reference to the source document in use
+fn get_document() -> Result<&'static String> {
+    unsafe {
+        SOURCE_DOCUMENT
+            .as_ref()
+            .ok_or(anyhow!("Attempt to read source document which is not set"))
+    }
 }
 
-fn get_line_number(pos: BytePosition) -> usize {
-    let line_start = get_line_start(pos);
-    let lines: Vec<char> = get_document()
+/// Finds the line number of a given source code byte position
+fn get_line_number(position: BytePosition) -> Result<usize> {
+    let line_start = get_line_start(position)?;
+    let lines: Vec<char> = get_document()?
         .chars()
         .take(line_start)
         .filter(|c| *c == '\n')
         .collect();
 
-    lines.len() + 1
+    Ok(lines.len() + 1)
 }
 
-fn get_line_start(pos: BytePosition) -> BytePosition {
-    let chop: Vec<char> = get_document().chars().take(pos).collect();
+/// Finds the start of the line this byte position is on
+fn get_line_start(position: BytePosition) -> Result<BytePosition> {
+    let chop: Vec<char> = get_document()?.chars().take(position).collect();
     let mut count = 0;
 
     for c in chop.iter().rev() {
@@ -42,24 +87,5 @@ fn get_line_start(pos: BytePosition) -> BytePosition {
         };
     }
 
-    pos - count
-}
-
-#[derive(Clone, Debug, PartialEq)]
-pub struct SourcePositionData {
-    pub s: BytePosition,
-    pub e: BytePosition,
-}
-
-impl Display for SourcePositionData {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "[{},{}]-[{},{}]",
-            get_line_number(self.s),
-            get_column(self.s),
-            get_line_number(self.e),
-            get_column(self.e)
-        )
-    }
+    Ok(position - count)
 }
